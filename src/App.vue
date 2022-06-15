6d1bf5f5192790efeff34986751a4bf448587d5e
gastonAr1985 <gastonarbert@gmail.com>
2022-06-15 17:36:54 -0300

    prueba

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="container-fluid">
+ <div class="jumbotron">
+  <div id="app">
+    <Formulario/>
+
+  </div>
+ </div>
+</div>
+
+</template>
+
+<script>
+import Formulario from './componentes/Formulario.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+
+    Formulario
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
